5b9172ec63bece404bd5e091c2e8bc8c1bff0b21
qwertyuiop8899 <217308952+qwertyuiop8899@users.noreply.github.com>
2025-09-25 21:12:13 +0000

    feat(normalize): add "test"

--- src/providers/animesaturn-provider.ts
+++ src/providers/animesaturn-provider.ts
@@ -216,6 +216,8 @@ function normalizeTitleForSearch(title: string): string {
     "Slam Dunk: Roar!! Basket Man Spirit": "Slam Dunk: Hoero Basketman-damashii! Hanamichi to Rukawa no Atsuki Natsu",
     "Slam Dunk: Shohoku Maximum Crisis! Burn Sakuragi Hanamichi": "Slam Dunk: Shouhoku Saidai no Kiki! Moero Sakuragi Hanamichi",
     "Slam Dunk: National Domination! Sakuragi Hanamichi": "Slam Dunk: Zenkoku Seiha Da! - Sakuragi Hanamichi",
+        "test": "tests",
+
     // << AUTO-INSERT-GENERIC >> (non rimuovere questo commento)
     // Qui puoi aggiungere altre normalizzazioni custom (legacy placeholder)
   };
