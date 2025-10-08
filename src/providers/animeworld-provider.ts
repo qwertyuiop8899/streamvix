2477bff88a1d1a45632ac7cb5acde310dc03b464
rossy92 <60351669+rossy92@users.noreply.github.com>
2025-10-08 10:22:13 +0000

    feat(normalize-aw): add "Cat's♥Eye"

--- src/providers/animeworld-provider.ts
+++ src/providers/animeworld-provider.ts
@@ -166,6 +166,10 @@ const exactMap: Record<string, string> = {
       "Cat's Eye (2025)": "Occhi di gatto (2025)",
 
 
+      "Cat's\u2665Eye": "Occhi di gatto (2025)",
+
+
+
   // << AUTO-INSERT-EXACT >> (non rimuovere questo commento)
 };
 // ==== AUTO-NORMALIZATION-EXACT-MAP-END ====
