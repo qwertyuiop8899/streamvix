8a7fa9975fdf550c1db87d565799e9f5b5d2e7f2
rossy92 <60351669+rossy92@users.noreply.github.com>
2026-01-14 09:13:27 +0000

    feat(normalize): add "Hell's Paradise"

--- src/providers/animesaturn-provider.ts
+++ src/providers/animesaturn-provider.ts
@@ -230,6 +230,13 @@ function normalizeTitleForSearch(title: string): string {
 
 
 
+        "Hell's Paradise": "Jigokuraku",
+
+
+
+
+
+
     // << AUTO-INSERT-EXACT >> (non rimuovere questo commento)
   };
   // ==== AUTO-NORMALIZATIOmN-EXACT-MAP-END ====
