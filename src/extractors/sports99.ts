9ab85baad0080b46fa95c317663a6ce477181cb9
qwertyuiop8899 <217308952+qwertyuiop8899@users.noreply.github.com>
2026-02-10 19:11:28 +0100

    Update sports99.ts

--- src/extractors/sports99.ts
+++ src/extractors/sports99.ts
@@ -255,7 +255,7 @@ export class Sports99Client {
             url = url.replace(/plan=vip/g, `plan=${this.plan}`);
             const headers = {
                 Referer: this.playerReferer,
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
             };
             const res = await axios.get(url, { headers, timeout: this.timeout });
             const js = this.decodeObfuscatedJs(res.data);
