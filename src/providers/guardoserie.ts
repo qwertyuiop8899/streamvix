fd073203874df51acbccf2bcee4eaf6af8e3658e
qwertyuiop8899 <217308952+qwertyuiop8899@users.noreply.github.com>
2026-03-10 18:11:32 +0100

    fix: timeout gus

--- src/providers/guardoserie.ts
+++ src/providers/guardoserie.ts
@@ -21,6 +21,7 @@ function createClient(useProxy: boolean) {
     const config = {
         jar,
         httpsAgent,
+        timeout: 10000,
         proxy: false as false,
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
