132d98de4420d55b1d6c8d9be56b8a464d4f9117
qwertyuiop8899 <217308952+qwertyuiop8899@users.noreply.github.com>
2026-07-31 09:41:13 +0200

    fix: adn-provider.ts

--- src/providers/adn-provider.ts
+++ src/providers/adn-provider.ts
@@ -26,7 +26,7 @@ export interface AdnConfig {
     cookie?: string;
 }
 
-const BASE_URL = 'https://altadefinizionestreaming.com';
+const BASE_URL = 'https://altadefinizionestreaming.tv';
 const USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36';
 
 function getCookie(config?: AdnConfig): string {
